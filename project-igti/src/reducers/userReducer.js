144c9f8c7b65445f2da119bfbf1382e4afa420f6
Rodrigo Matos <rodrigo.matos@armateus.com.br>
2018-05-24 19:41:31 -0300

    atualizar

--- project-igti/src/reducers/userReducer.js
+++ project-igti/src/reducers/userReducer.js
@@ -1,9 +1,11 @@
 const INITIAL_STATE = {
-    all: []
+    contacts: []
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {     
+        case 'ADD-CONTACT':
+            return { ...state, contacts: action.payload }
         default:
             return state
     }
--- project-igti/src/routes/User/components/User.js
+++ project-igti/src/routes/User/components/User.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const User = () => (
-  <div > 
+import { Form, FormGroup, FormControl, Button, Panel, Image } from 'react-bootstrap'
 
-  </div>
+export const User = () => (
+
+  <Panel className="painel-cadastro"  style={{width:'40%', margin: '20px auto', textAlign:'center'}}>
+    <Form>
+      <div style={{width:'100px', height:'100px', backgroundColor:'#cccc', margin:'0 auto'}}/>
+      <FormControl type="text" placeholder="Digite seu nome" style={{ margin: '10px auto', width:'90%' }} />
+      <FormControl type="text" placeholder="Digite seu e-mail" style={{ margin: '10px auto', width:'90%' }}/>
+      <Button bsStyle="primary" bsSize="medium" type="submit" active style={{  clear:'both',margin: '10px 20px', float:'right'}}> Cadastrar</Button>
+    </Form>
+  </Panel>
+
 )
 
 export default User
